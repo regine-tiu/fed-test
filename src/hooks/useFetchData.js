1d63041e3754b97e09e8acc16f63b7aa96436199
regine_tiu <regine.d.tiu@gmail.com>
2022-03-25 18:27:09 -0400

    created custom Fetch Data hook

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,26 @@
+import { useEffect, useState} from 'react';
+import axios from "axios"; 
+
+const useFetchData = (endpoint) => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: response } = await axios.get(endpoint);
+        setData(response);
+      } catch (error) {
+        console.error(error)
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return {
+    data,
+  };
+};
+
+export default useFetchData;
+  
