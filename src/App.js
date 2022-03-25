e50692454e87fb453a8d98a1f9f59f59f40f86ba
regine_tiu <regine.d.tiu@gmail.com>
2022-03-25 18:27:36 -0400

    used custom hook to fetch data into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+
+import useFetchData from './hooks/useFetchData.js'
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const case_studies_ep = process.env.REACT_APP_CASE_STUDIES;
+  const cat_ep = process.env.REACT_APP_CATEGORIES;
+  
+  const case_studies = useFetchData(case_studies_ep);
+  const categories = useFetchData(cat_ep);
+
+  console.log(case_studies);
+  console.log(categories);
+
   return (
     <div className="App">
       <header className="App-header">
+       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
