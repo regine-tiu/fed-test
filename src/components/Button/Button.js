90861e47fa4f6d40e4a355a6027bf5d4cb173512
regine_tiu <regine.d.tiu@gmail.com>
2022-03-26 18:19:19 -0400

    added animation to project btn

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,10 @@
 import arrow from "../../assets/arrow-icon.svg"
+import styles from "./Button.module.sass"
 
 const Button = () => {
+    const {slide_fx} = styles;
     return (
-        <button>View case study
+        <button className={slide_fx}>View case study
             <img src={arrow} alt="arrow icon"/>
         </button>
     )
