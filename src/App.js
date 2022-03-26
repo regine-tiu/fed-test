6481b3e561019ad8eeba7a046227e1bd967537ac
regine_tiu <regine.d.tiu@gmail.com>
2022-03-26 18:54:41 -0400

    added assets svgs to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
 import useFetchData from './hooks/useFetchData.js'
 import './global.sass';
-import header_asset from "./assets/asset2.svg"
 import ProjectCard from './components/ProjectCard';
+import header_asset from "./assets/asset2.svg"
+import footer_asset from "./assets/asset3.svg"
+import sidebar_asset from "./assets/asset1.svg"
+
 
 const case_studies_ep = process.env.REACT_APP_CASE_STUDIES;
 const cat_ep = process.env.REACT_APP_CATEGORIES;
@@ -23,25 +26,31 @@ function ProjectPage() {
    setActiveCS( cs_arr?.filter( cs => cs.categories[0].slug === label))
   },[label]);
 
-  useEffect(() => {
+  useEffect(() => { 
     setCat(cat_arr)
     setActiveCS(cs_arr)
   },[]);
 
-return (
+return ( 
     <div className='page'>
-      <img src={header_asset} alt=""/> 
+      <div className='asset-top'>
+        <img src={header_asset} alt=""/>
+      </div>
       <div className="page-wrapper">
-        <header className="page-header">
+        <header className="page-header"> 
         <h1>Work</h1>
         </header>
-        <ul className='cat-row'>
+        <ul className='cat-row'> 
           {cat?.map(cat => <li><button className='cat-btn' key={cat.id} onClick={() => setLabel(cat.slug)}>{cat.title}</button></li>)}
         </ul>
         <hr className='cat-after-line'></hr>
         <div className='project-gallery'>
           {activeCS?.map(project => <ProjectCard project={project} key={project.id} />)}
         </div>
+      </div>
+      <div className='asset-bottom'>
+        <img src={footer_asset} alt="" className='dot-circle'/>
+        <img src={sidebar_asset} alt="" className='half-circle'/>
       </div>
     </div>
   );
