672593a0f384b1eea576028464e856f97bbbadfa
regine_tiu <regine.d.tiu@gmail.com>
2022-03-26 10:39:15 -0400

    added .map function to map through case studies arr

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
 import useFetchData from './hooks/useFetchData.js'
 import './global.sass';
 import header_asset from "./assets/asset2.svg"
 
 
-function App() {
-  const case_studies_ep = process.env.REACT_APP_CASE_STUDIES;
-  const cat_ep = process.env.REACT_APP_CATEGORIES;
-  
-  const case_studies = useFetchData(case_studies_ep);
-  const categories = useFetchData(cat_ep);
+const case_studies_ep = process.env.REACT_APP_CASE_STUDIES;
+const cat_ep = process.env.REACT_APP_CATEGORIES;
 
-  console.log(case_studies);
-  console.log(categories);
+function ProjectPage() {  
+  const cs_data = useFetchData(case_studies_ep);
+  const cat_data = useFetchData(cat_ep);
+  const cs_arr = cs_data['data']['case-studies']
+  const cat_arr = cat_data['data']['categories']
+
+ console.log(cs_arr)
 
   return (
     <div className='page'>
@@ -21,12 +21,12 @@ function App() {
       <header className="page-header">
        <h1>Work</h1>
       </header>
-
-      
-      
+      <div className='project-gallery'>
+        {cs_arr.map(project => <div className='project-card'><img src={project.thumbnail} alt="project thumbnail"/></div>)}
+      </div>
       </div>
     </div>
   );
 }
 
-export default App;
+export default ProjectPage;
